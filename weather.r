54fe9a9ba5a47fb53915011054847f123c29cb72
EMILY GAO <EMILY.Y.GAO@GMAIL.COM>
2016-04-21 17:46:00 -0400

    nothing changed

--- weather.r
+++ weather.r
@@ -9,11 +9,13 @@ library(gridExtra)
 library(ggrepel)
 library(rwunderground)
 
-# wunderground key: dc1f7cb13c35da6d
+# wunderground key (Nina): dc1f7cb13c35da6d
+# wunderground key (Emily): c52d7ad36cc9d20f
+# wunderground key (Jon): 27e1fb02aa0bc179
 
 # create date range
-date.range <- seq.Date(from=as.Date('2014-1-01'), 
-                       to=as.Date('2014-1-05'), by='1 day')
+date.range <- seq.Date(from=as.Date('2015-6-20'), 
+                       to=as.Date('2016-4-20'), by='1 day')
 
 # remove "-" from date range
 date.range <- str_replace_all(date.range, "[[:punct:]]", "")
@@ -27,8 +29,14 @@ for (i in seq_along(date.range)) {
   print(date.range[i])
   ny[[i]] <- history_daily(set_location(
     territory = "New York", city = "New York"), date.range[i])
+  Sys.sleep(10)
 }
 
 # stack loop responses for weather data
 weather_NewYork <- ldply(ny)
+
+# save new york data as .csv
+write.csv(weather_NewYork_2016, file = "weather_NewYork.csv", row.names=FALSE)
+
+
 
