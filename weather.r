f8cc97d7b0bd04865acfcef558647c45a378f85a
Nina Punukollu <ninapunukollu@Ninas-MacBook-Air.local>
2016-04-21 17:27:46 -0400

    first data pull

--- weather.r
+++ weather.r
@@ -1,3 +1,8 @@
+# set working directory
+
+# Nina
+setwd("~/Documents/HSPH/DataScience/sales-predict-")
+
 library(plyr)
 library(dplyr)
 library(knitr)
@@ -9,11 +14,12 @@ library(gridExtra)
 library(ggrepel)
 library(rwunderground)
 
-# wunderground key: dc1f7cb13c35da6d
+# wunderground key (Nina): dc1f7cb13c35da6d
+# wunderground key (Emily): c52d7ad36cc9d20f
 
 # create date range
-date.range <- seq.Date(from=as.Date('2014-1-01'), 
-                       to=as.Date('2014-1-05'), by='1 day')
+date.range <- seq.Date(from=as.Date('2015-6-20'), 
+                       to=as.Date('2016-4-20'), by='1 day')
 
 # remove "-" from date range
 date.range <- str_replace_all(date.range, "[[:punct:]]", "")
@@ -22,13 +28,18 @@ date.range <- str_replace_all(date.range, "[[:punct:]]", "")
 ny <- vector(mode='list', length=length(date.range))
 
 # pull weather data from wunderground for all dates in date range
+# pause for 10 seconds after each iteration (API only allows 10 requests per minute)
 # city = New York
 for (i in seq_along(date.range)) {
   print(date.range[i])
   ny[[i]] <- history_daily(set_location(
     territory = "New York", city = "New York"), date.range[i])
+  Sys.sleep(10)
 }
 
 # stack loop responses for weather data
 weather_NewYork <- ldply(ny)
+
+# save new york data as .csv
+write.csv(weather_NewYork_2016, file = "weather_NewYork.csv", row.names=FALSE)
 
