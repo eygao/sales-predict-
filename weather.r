ce8946e4804d3a513661f7f19968190d545da41c
EMILY GAO <EMILY.Y.GAO@GMAIL.COM>
2016-04-25 17:13:02 -0400

    added code for NY year 3 but no successful run yet

--- weather.r
+++ weather.r
@@ -17,6 +17,7 @@ library(rwunderground)
 # wunderground key (Nina): dc1f7cb13c35da6d
 # wunderground key (Emily): c52d7ad36cc9d20f
 # wunderground key (Jon): 27e1fb02aa0bc179
+# wunderground key (Rachel): 11cd5ba513f919d8
 
 #######################################################################
 ################## YEAR 1: NEW YORK, NEW YORK #########################
@@ -77,6 +78,36 @@ weather_NewYork <- ldply(ny)
 
 # save new york data as .csv
 write.csv(weather_NewYork, file = "weather_NewYork_04-04_2014-15.csv", row.names=FALSE)
+
+#######################################################################
+################## YEAR 3: NEW YORK, NEW YORK #########################
+#######################################################################
+
+# create date range
+date.range <- seq.Date(from=as.Date('2013-4-20'), 
+                       to=as.Date('2014-4-19'), by='1 day')
+
+# remove "-" from date range
+date.range <- str_replace_all(date.range, "[[:punct:]]", "")
+
+# create empty vector to store dates
+ny <- vector(mode='list', length=length(date.range))
+
+# pull weather data from wunderground for all dates in date range
+# pause for 10 seconds after each iteration (API only allows 10 requests per minute)
+# city = New York
+for (i in seq_along(date.range)) {
+  print(date.range[i])
+  ny[[i]] <- history_daily(set_location(
+    territory = "New York", city = "New York"), date.range[i])
+  Sys.sleep(10)
+}
+
+# stack loop responses for weather data
+weather_NewYork <- ldply(ny)
+
+# save new york data as .csv
+write.csv(weather_NewYork, file = "weather_NewYork_04-04_2013-14.csv", row.names=FALSE)
 
 #######################################################################
 ################# YEAR 1: LOS ANGELES, CALIFORNIA #####################
